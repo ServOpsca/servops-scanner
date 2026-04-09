b4355a144c297ceec68db26112b7dfcafc368af4
Servopsca <Servopsca@gmail.com>
2026-04-08 22:42:05 -0400

    add supabase client

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -116,7 +116,7 @@ function App() {
       <section id="spacer"></section>
     </>
   )
-}
+} */
 
 export default function App() {
   return (
